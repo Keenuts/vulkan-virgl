be30598bcfe440310cf8bea7f3ed60e8ada38f7d
Nathan Gauër <nathan@gauer.org>
2018-07-13 17:42:48 +0200

    run script refactorized.
    Signed-off-by: Nathan Gauër <nathan@gauer.org>

--- run-demo.sh
+++ run-demo.sh
@@ -14,12 +14,14 @@ DEFAULT_BRANCH_APP="master"
 function show_help()
 {
     echo "vulkan-virgl-helper:"
-    echo "  --mesa=   : override the URL for the mesa project"
-    echo "  --virgl=  : override the URL for the virglrenderer project"
-    echo "  --app=    : override the URL for the vulkan-compute project"
-    echo "  -u        : only update the repositories"
-    echo "  -f        : remove the build folder. Forcing updates."
-    echo "  -ns       : disable sync step."
+    echo "  --url-mesa=   : override the URL for the mesa project"
+    echo "  --url-virgl=  : override the URL for the virglrenderer project"
+    echo "  --url-app=    : override the URL for the vulkan-compute project"
+    echo "  -c        : disable repo cloning step"
+    echo "  -p        : disable repo pull step"
+    echo "  -b        : disable building step, implies -r"
+    echo "  -r        : disable running step"
+    echo "  -f        : remove the build folder first (force)"
 }
 
 # Takes two parameters:
@@ -37,17 +39,42 @@ function clone_repo()
     echo -e "${COLOR_BEGIN}[GIT] checking ${branch}${COLOR_END}"
 
     if [ ! -d "$folder" ]; then
-        echo -e "${COLOR_BEGIN}[GIT] cloning ${url} ${branch}${COLOR_END}"
+        echo -e "${COLOR_BEGIN}\tcloning ${url} ${branch}${COLOR_END}"
         git clone "$url" "$folder" -b "$branch" --depth=1 || exit 1
     else
-        cd "$folder"
-        if $update_remote; then
-            echo -e "${COLOR_BEGIN}[GIT] changing remote to ${url}${COLOR_END}"
-            git remote set-url origin "$url"
-        fi
-        git pull
+        echo -e "${COLOR_BEGIN}\trepo $(basename ${folder}) already cloned.${COLOR_END}"
     fi
 
+    cd "$root"
+}
+
+# takes 2 params
+#  - REPO-DIR
+#  - REMOTE-URL
+#
+# Will update the remote on the given repo
+function set_remote()
+{
+    folder="$1"
+    url="$2"
+
+    echo -e "${COLOR_BEGIN}[GIT] changing remote to ${url}${COLOR_END}"
+    cd "$folder"
+    git remote set-url origin "$url"
+    cd "$root"
+}
+
+# takes 1 param
+#  - REPO-DIR
+#
+# Will do a 'git pull' in the given repo
+function pull_repo()
+{
+    folder="$1"
+
+    echo -e "${COLOR_BEGIN}[GIT] pulling changes for ${folder}${COLOR_END}"
+    cd "$folder"
+    git pull
     cd "$root"
 }
 
@@ -65,12 +92,13 @@ function build_mesa()
     fi
 
     echo -e "${COLOR_BEGIN}[INFO] building MESA${COLOR_END}"
-    ninja -C mesa/build -j $(( $(nproc) * 2 ))
+    ninja -C mesa/build
 
     echo -e "${COLOR_BEGIN}[INFO] locating ICD files${COLOR_END}"
     export ICD_JSON="$(realpath mesa/build/src/virgl/virglrenderer_debug.x86_64.json)"
+
     if [ ! -f "$ICD_JSON" ]; then
-        echo "ERROR: ICD json file not found. Existing now"
+        echo "ERROR: ICD json file not found. Exiting now"
         exit 1
     fi
 
@@ -86,14 +114,20 @@ function build_virglrenderer()
         ./autogen.sh                    \
             --with-vulkan               \
             --enable-debug              \
-            --enable-tests              \
             --prefix=$(realpath build) > /dev/null
     fi
 
     echo -e "${COLOR_BEGIN}[INFO] building virglrenderer${COLOR_END}"
     make -j $(( $(nproc) * 2 )) > /dev/null
+    make install > /dev/null
 
-    cp vtest/virgl_test_server "$root/"
+    echo -e "${COLOR_BEGIN}[INFO] locating vtest server binary${COLOR_END}"
+    export VIRGL_SERVER_BIN="$(realpath build/bin/virgl_test_server)"
+    if [ ! -f "$VIRGL_SERVER_BIN" ]; then
+        echo "ERROR: Vtest server's binary not found. Exiting now"
+        exit 1
+    fi
+
     cd "$root"
 }
 
@@ -110,15 +144,25 @@ function build_vulkan_compute()
     echo -e "${COLOR_BEGIN}[INFO] building vulkan application${COLOR_END}"
     make -C build -j $(( $(nproc) * 2 ))
 
-    cp build/sum.spv "$root/sum.spv"
-    cp build/sum "$root/vulkan-application"
+    echo -e "${COLOR_BEGIN}[INFO] locating test application${COLOR_END}"
+    export VULKAN_COMPUTE_BINARY="$(realpath build/sum)"
+    export VULKAN_COMPUTE_SHADER="$(realpath build/sum.spv)"
+    if [ ! -f "$VULKAN_COMPUTE_BINARY" ]; then
+        echo "ERROR: test app's binary not found. Exiting now"
+        exit 1
+    fi
+    if [ ! -f "$VULKAN_COMPUTE_SHADER" ]; then
+        echo "ERROR: test app shader's not found. Exiting now"
+        exit 1
+    fi
     cd "$root"
 }
 
 function run_app()
 {
     echo -e "${COLOR_BEGIN}[INFO] running the application now.${COLOR_END}"
-    ./virgl_test_server --no-fork &
+
+    $VIRGL_SERVER_BIN --no-fork &
     SERVER_PID=$!
 
     sleep 1
@@ -126,9 +170,13 @@ function run_app()
     export VULKAN_DRIVER=virpipe
     export USE_VIRTIOGPU=true
     export VK_ICD_FILENAMES="$ICD_JSON"
-    
+
     set +e
-    ./vulkan-application
+
+    cp -f $VULKAN_COMPUTE_SHADER .
+    cp -f $VULKAN_COMPUTE_BINARY .
+    ./$(basename $VULKAN_COMPUTE_BINARY)
+
     set -e
 
     echo -e "${COLOR_BEGIN}[INFO] Killing the vtest server.${COLOR_END}"
@@ -142,21 +190,24 @@ mesa_url="$DEFAULT_URL_MESA"
 virglrenderer_url="$DEFAULT_URL_VIRGL"
 vulkan_compute_url="$DEFAULT_URL_APP"
 
-run_git_step=true
-update_only=false
+step_clone=true
+step_pull=true
+step_build=true
+step_run=true
+
 update_remote=false
 force_clean=false
 
 while [ "$#" -gt 0 ]; do
     case "$1" in
-        --mesa=*)   mesa_url="${1#*=}";             update_remote=true; shift 1;;
-        --virgl=*)  virglrenderer_url="${1#*=}";    update_remote=true; shift 1;;
-        --app=*)    vulkan_compute_url="${1#*=}";   update_remote=true; shift 1;;
-
-        -u) update_only=true; shift 1;;
+        --url-mesa=*)   mesa_url="${1#*=}";             update_remote=true; shift 1;;
+        --url-virgl=*)  virglrenderer_url="${1#*=}";    update_remote=true; shift 1;;
+        --url-app=*)    vulkan_compute_url="${1#*=}";   update_remote=true; shift 1;;
+        -c) step_clone=false; shift 1;;
+        -p) step_pull=false; shift 1;;
+        -b) step_build=false; step_run=false; shift 1;;
+        -r) step_run=false; shift 1;;
         -f) force_clean=true; shift 1;;
-        -ns) run_git_step=false; shift 1;;
-
         *) show_help; exit 1;;
     esac
 done
@@ -182,21 +233,34 @@ mkdir -p build
 cd build
 export root="$(pwd)"
 
-if $run_git_step; then
+if $step_clone; then
     # cloning repos
     clone_repo "$mesa_url"            "$DEFAULT_BRANCH_MESA"   mesa
     clone_repo "$virglrenderer_url"   "$DEFAULT_BRANCH_VIRGL"  virglrenderer
     clone_repo "$vulkan_compute_url"  "$DEFAULT_BRANCH_APP"    vulkan-compute
-
-    if $update_only; then
-        exit 0
-    fi
 fi
 
-# building
-build_mesa
-build_virglrenderer
-build_vulkan_compute
+if $update_remote; then
+    set_remote "mesa"           "$mesa_url"
+    set_remote "virglrenderer"  "$virglrenderer_url"
+    set_remote "vulkan-compute" "$vulkan_compute_url"
+    remote_update
+fi
 
-# running
-run_app
+if $step_pull; then
+    pull_repo mesa
+    pull_repo virglrenderer
+    pull_repo vulkan-compute
+fi
+
+if $step_build; then
+    # building
+    build_mesa
+    build_virglrenderer
+    build_vulkan_compute
+fi
+
+if $step_run; then
+    # running
+    run_app
+fi
